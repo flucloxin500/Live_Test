6bebfdf64602ef6d40a3eb3f84ea599485586ef9
flucloxin500 <mdwalid826@gmail.com>
2023-09-27 00:20:43 +0600

    Live Test of module seven

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,105 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-      title: 'Ostad App',
+      home: CounterScreen(),
     );
   }
 }
+class CounterScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return CounterState();
+  }
+}
 
-class HomeScreen extends StatelessWidget{
+class CounterState extends State<CounterScreen> {
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          actions: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(Icons.shopping_cart),
-            ),
-          ],
-
-          title: Text('My Shopping List'),
-          centerTitle: true,
-        ),
-        body:Column(
+      appBar: AppBar(
+        title: Text('Counter App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ListTile(
-              leading: Icon(Icons.shopping_bag),
-              title: Text('OneUI',
+            Text('Count:'),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: Text(counter.toString(),
                 style: TextStyle(
+                  fontSize: 55,
                   fontWeight: FontWeight.bold,
-                ),),
+                ),
+              ),
             ),
-            ListTile(
-              leading: Icon(Icons.shopping_bag),
-              title: Text('iOS',
-                style: TextStyle(
-                  fontWeight: FontWeight.w300,
-                ),),
-            ),
-            ListTile(
-              leading: Icon(Icons.shopping_bag),
-              title: Text('Octopus OS'),
-            ),
-            ListTile(
-              leading: Icon(Icons.shopping_bag),
-              title: Text('MIUI'),
-            ),
-            ListTile(
-              leading: Icon(Icons.shopping_bag),
-              title: Text('Oxygen OS'),
-            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: (){
+                  setState(() {
+                    if ( counter < 5)
+                    {
+                      counter = counter + 1;
+                    }
+                    else
+                    {
+                      showDialog(context: context, builder: (context){
+                        return AlertDialog(
+                          title: Text('Button Pressed ${counter} times',
+                            style: TextStyle(
+                                fontWeight: FontWeight.normal
+                            ),),
+                          actions: [
+                            TextButton(onPressed: (){},
+                              child: Text('Close'),),
+                          ],
+                        );
+                      }
+                      );
+                    }
 
+                  });
+                },
+                  child: Text('+',
+                    style: TextStyle(
+                        fontSize: 25
+                    ),),),
+
+                Padding(
+                  padding: const EdgeInsets.only(left: 18),
+                  child: ElevatedButton(onPressed: (){
+
+                    setState(() {
+                      if ( counter > 0)
+                      {
+                        counter = counter - 1;
+                      }
+
+                    });
+                  },
+                    child: Text('-',
+                      style: TextStyle(
+                          fontSize: 25
+                      ),),),
+                ),
+              ],
+            )
           ],
-        )
+        ),
 
-
+      ),
     );
   }
-
-}
-void main (){
-  runApp(MyApp());
-
 }
